b46aa56205eb088c67d212ebb2e2645777404ac5
Robert <robert.david.west@gmail.com>
2015-05-18 15:13:05 -0400

    completed FlaskTaskr, Part2 - initial conversion to SQLAlchemy and implemented data validation

--- flasktaskr/app/db_create.py
+++ flasktaskr/app/db_create.py
@@ -1,27 +1,17 @@
 # db_create.py
 
-import sqlite3
+from views import db
+from models import Task
+from datetime import date
+
+# create the database and the db table
+
 from config import DATABASE_PATH
+db.create_all()
 
-create_table = False
+# insert data
+db.session.add(Task("Finish this tutorial", date(2014, 3, 13), 10, 1))
+db.session.add(Task("Finish Real Python", date(2014, 3, 13), 10, 1))
 
-with sqlite3.connect(DATABASE_PATH) as connection:
-
-    # get a cursor object used to execute SQL commands
-    c = connection.cursor()
-
-    if create_table:
-        # create the table \
-        c.execute("""CREATE TABLE tasks(
-        task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        due_date TEXT NOT NULL,
-        priority INTEGER NOT NULL,
-        status INTEGER NOT NULL)""")
-
-    # insert dummy data into the table
-    c.execute('INSERT INTO tasks (name, due_date, priority, status)'
-              'VALUES("Finish this tutorial", "05/17/2015", 10, 1)')
-
-    c.execute('INSERT INTO tasks (name, due_date, priority, status)'
-              'VALUES("Finish Real Python Course 2", "05/17/2015", 10, 1)')
+# commit the changes
+db.session.commit()
--- flasktaskr/app/forms.py
+++ flasktaskr/app/forms.py
@@ -7,8 +7,10 @@ from wtforms.validators import DataRequired
 class AddTaskForm(Form):
     task_id = IntegerField('Priority')
     name = TextField('Task Name', validators=[DataRequired()])
-    due_date = DateField('Due Date (mm/dd/yyyy)', validators=[DataRequired()],
-                         format='%m%d%Y')
+    due_date = DateField(
+        'Due Date (mm/dd/yyyy)',
+        validators=[DataRequired()], format='%m/%d/%Y'
+    )
     priority = SelectField('Priority',
                            validators=[DataRequired()],
                            choices=[('1', '1'), ('2', '2'), ('3', '3'),
