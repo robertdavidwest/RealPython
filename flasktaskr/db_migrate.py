a5295d02127895f06befe9e1dd53c30fa18ae7da
Robert <robert.david.west@gmail.com>
2015-08-10 13:58:49 -0400

    moved db_create.py and db_migrate.py out of the app folder

--- flasktaskr/db_migrate.py
+++ flasktaskr/db_migrate.py
@@ -1,10 +1,10 @@
 # db_migrate.py
 
 from views import db
-from datetime import datetime
+# from datetime import datetime
 from config import DATABASE_PATH
 import sqlite3
-
+'''
 with sqlite3.connect(DATABASE_PATH) as connection:
 
     # get a cursor object used to execute SQL commands
@@ -30,3 +30,28 @@ with sqlite3.connect(DATABASE_PATH) as connection:
 
     # delete old tasks table
     c.execute('DROP TABLE old_tasks')
+'''
+
+with sqlite3.connect(DATABASE_PATH) as connection:
+
+    # get a cursor object used to execute SQL commands
+    c = connection.cursor()
+
+    # temporarily change the name of the user table
+    c.execute("""ALTER TABLE users RENAME TO old_users""")
+
+    # recreate a new users table with updated schema
+    db.create_all()
+
+    # retrieve data from old_users table
+    c.execute("""SELECT name, email, password FROM old_users ORDER BY id ASC""")
+
+    # save all rows as a list of tuples; set role to 'user'
+    data = [(row[0], row[1], row[2], 'user') for row in c.fetchall()]
+
+    # insert data to users table
+    c.executemany("""INSERT INTO users (name, email, password, role)
+                  VALUES (?, ?, ?, ?)""", data)
+
+    # delete old_users table
+    c.execute("DROP TABLE old_users")
